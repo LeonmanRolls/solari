0dbcae62b522869e0d551facf583a7b4a2ce4699
Leon Talbert <leon.talbert@gmail.com>
2015-05-22 19:42:13 +0100

    adding the ful screen modal

--- src/cljs/solari/views/residential.cljs
+++ src/cljs/solari/views/residential.cljs
@@ -9,72 +9,66 @@
 
 (enable-console-print!)
 
-(def res-atom (atom {:projects [{:id "project-01"
+(def res-atom (atom {:text "Homes are personal projects and we love that. When we take on a residential project we take on the thoughts, feelings, personality and unique circumstances of the client. We work closely with you to ensure that your home is exactly that – yours. You’re with us every step of the way, this not only makes absolute sense but undoubtedly delivers the best results. We share the challenges and successes with you and make you the expert of your own project by going at a pace that promotes attention to detail and clarity of thought from start to finish."
+                     :projects [{:id "project-01"
                                  :title "Wadestown Renovation"
-                                 :thumbail "/img/wadeston.jpg"}
+                                 :thumbail "/img/wadestown.jpg"}
 
                                 {:id "project-02"
                                  :title "Lyall bay renovation"
-                                 :thumbnail ""}
+                                 :thumbnail "/img/lyall.jpg"}
 
                                 {:id "project-03"
                                  :title "Catline Lane Subdivision"
-                                 :thumbnail: ""}
-
-                                ] }))
+                                 :thumbnail: "/img/another.jpg"}]}))
 
 (defn project-tumbnail [data owner]
   (reify
     om/IRender
     (render [this]
-      (dom/li nil
+      (do
+       (println "data: " data)
+       (dom/li nil
               (dom/figure nil
                           (dom/div nil
                                    (dom/img #js {:src (:thumbnail data)})
                                    (dom/figcaption nil
                                                    (dom/h3 nil (:title data))
-                                                   (dom/a #js {:href "#"} "Take a look")))))
-      )
+                                                   (dom/a #js {:href (str "#" (:id data) "-modal")} "Take a look")))))))))
 
 
-    )
+(defn project-modal [data owner]
+  (reify
+    om/IDidMount
+    (did-mount [this]
+      (do
+        (js/alert "did mount")
+        (.-animatedModal (js/$ (str "#" (:id data) "-modal")))))
+    om/IRender
+    (render [this]
+      (dom/div #js {:id (str (:id data) "-modal")}
+               (dom/div #js {:id "btn-close-modal" :className (str "close-" (:id data) "-modal")})
+               (dom/div #js {:className "modal-content"})))))
 
-  )
 
 (defn residential-page [data owner]
   (reify
     om/IRender
     (render [this]
+
+      (apply dom/div #js {:id "modals"}
+             (om/build-all project-modal (:projects data)))
+
+
       (dom/div nil
-               (dom/p #js {:className "text-area"} "Homes are personal projects and we love that. When we take on a residential project we take on the thoughts, feelings, personality and unique circumstances of the client. We work closely with you to ensure that your home is exactly that – yours. You’re with us every step of the way, this not only makes absolute sense but undoubtedly delivers the best results. We share the challenges and successes with you and make you the expert of your own project by going at a pace that promotes attention to detail and clarity of thought from start to finish.")
+               (dom/p #js {:className "text-area"} (:text data))
                (dom/div #js {:className "row"}
-                        (dom/ul #js{:className "grid cs-style-4"} nil
-                                (dom/li nil
-                                        (dom/figure nil
-                                                    (dom/div nil
-                                                             (dom/img #js {:src "/img/wadestown.jpg"})
-                                                             (dom/figcaption nil
-                                                                             (dom/h3 nil "Wadestown Renovation")
-                                                                             (dom/a #js {:href "#"} "Take a look")))))
 
-                                (dom/li nil
-                                        (dom/figure nil
-                                                    (dom/div nil
-                                                             (dom/img #js {:src "/img/lyall.jpg"})
-                                                             (dom/figcaption nil
-                                                                             (dom/h3 nil "Lyall bay renovation")
-                                                                             (dom/a #js {:href "#"} "Take a look")))))
-
-                                (dom/li nil
-                                        (dom/figure nil
-                                                    (dom/div nil
-                                                             (dom/img #js {:src "/img/lyall.jpg"})
-                                                             (dom/figcaption nil
-                                                                             (dom/h3 nil "Catlina Lane Subdivision")
-                                                                             (dom/a #js {:href "#"} "Take a look")))))))))))
+                        (apply dom/ul #js {:className "grid cs-style-4"}
+                                       (om/build-all project-tumbnail (:projects data))))))))
 
 (defn residential-init []
-  (om/root residential-page {}
+  (om/root residential-page res-atom
                      {:target (. js/document (getElementById "main-content-container"))}))
 
 
