0a09944a6f1d18f15851f771860871ed47fba7f1
Leon Talbert <leon.talbert@gmail.com>
2015-06-15 03:28:39 +0800

    process is better now

--- src/cljs/solari/views/process.cljs
+++ src/cljs/solari/views/process.cljs
@@ -13,12 +13,28 @@
 (def box-style #js {:style #js {:color "white" :marginTop "10px" :padding "20px" :border "2px solid #c0392b"
                                 :textTransform "uppercase"}})
 
+(def hipster-data [{:href "" :label "Short version" :id "short-li"
+                    :callback #(do (ef/at "#short-version" (ef/remove-class "hidden"))
+                                   (ef/at "#long-version" (ef/add-class "hidden"))
+                                   (ef/at "#short-li" (ef/set-attr :color "red"))
+                                   (ef/at "#long-li" (ef/set-attr :color "none"))
+                                   )}
+
+                   {:href "" :label "Long version" :id "long-li"
+                    :callback #(do (ef/at "#short-version" (ef/add-class "hidden"))
+                                   (ef/at "#long-version" (ef/remove-class "hidden"))
+                                   (ef/at "#short-li" (ef/set-attr :color "none"))
+                                   (ef/at "#long-li" (ef/set-attr :color "red"))
+                                   )}])
+
 (defn process-page [data owner]
   (reify
 
     om/IDidMount
     (did-mount [this]
-      (js/accordion))
+      (js/accordion)
+      (ef/at "#short-li" (ef/set-attr :color "red"))
+      (ef/at "#long-li" (ef/set-attr :color "none")))
 
     om/IRenderState
     (render-state [this state]
@@ -26,7 +42,7 @@
         (dom/div nil
                  (apply dom/ul #js {:style #js {:top "100px" :width "140px" :right "0px" :position "fixed"
                                                 :listStyle "none" :borderBottom "1px solid white" :padding "0px" }}
-                        (om/build-all common/simple-li (:left-nav local)))
+                        (om/build-all common/simple-li hipster-data))
 
                  (om/build common/paragraph-partial local {:state {:color "white" :key :text}})
 
@@ -40,7 +56,7 @@
                             (if (:admin state) (om/build common/input-partial (:step3 locall)))))
 
                  (println "state" state)
-                 (dom/div #js {:id "long-version" :className "accordion"}
+                 (dom/div #js {:id "long-version" :className "accordion hidden"}
                           (apply dom/dl nil
                                  (om/build-all common/accordion-partial  (get local :long) {:state state}))))))))
 
